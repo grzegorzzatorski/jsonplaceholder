214e3361543c8d77864a9ef812707f59c2f184d8
gzatorski <gzatorski@debian.home>
2019-12-07 08:50:23 +0100

    Init project with gradle

---
+++ src/main/scala/com/gzatorski/jsonplaceholder/Launcher.scala
@@ -0,0 +1,10 @@
+package com.gzatorski.jsonplaceholder
+
+
+object Launcher {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
---
+++ src/test/scala/com/gzatorski/jsonplaceholder/LauncherSpec.scala
@@ -0,0 +1,9 @@
+package com.gzatorski.jsonplaceholder
+
+import org.scalatest.{Matchers, WordSpecLike}
+
+
+class MyTest extends Matchers with WordSpecLike {
+
+
+}
